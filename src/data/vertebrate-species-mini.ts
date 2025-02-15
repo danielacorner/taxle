9c38ac7cb84c2b1b0ee79bd3e7fecf694e777eee
daniel.corner <danielcorner7@gmail.com>
2025-02-15 02:15:35 -0500

    minilist

---
+++ src/data/vertebrate-species-mini.ts
@@ -0,0 +1,95 @@
+import { Species } from "../types/game";
+
+// Curated list of well-known vertebrate species
+export const vertebrateSpecies: Species[] = [
+  {
+    "scientificName": "Panthera tigris",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Tiger",
+    "genus": "Panthera",
+    "species": "tigris"
+  },
+  {
+    "scientificName": "Panthera leo",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Lion",
+    "genus": "Panthera",
+    "species": "leo"
+  },
+  {
+    "scientificName": "Canis lupus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Wolf",
+    "genus": "Canis",
+    "species": "lupus"
+  },
+  {
+    "scientificName": "Felis catus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cat",
+    "genus": "Felis",
+    "species": "catus"
+  },
+  {
+    "scientificName": "Gorilla gorilla",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Western_gorilla",
+    "genus": "Gorilla",
+    "species": "gorilla"
+  },
+  {
+    "scientificName": "Homo sapiens",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Human",
+    "genus": "Homo",
+    "species": "sapiens"
+  },
+  {
+    "scientificName": "Tyrannosaurus rex",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Tyrannosaurus",
+    "genus": "Tyrannosaurus",
+    "species": "rex"
+  },
+  {
+    "scientificName": "Ursus arctos",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Brown_bear",
+    "genus": "Ursus",
+    "species": "arctos"
+  },
+  {
+    "scientificName": "Balaenoptera musculus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Blue_whale",
+    "genus": "Balaenoptera",
+    "species": "musculus"
+  },
+  {
+    "scientificName": "Aquila chrysaetos",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Golden_eagle",
+    "genus": "Aquila",
+    "species": "chrysaetos"
+  },
+  {
+    "scientificName": "Python reticulatus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Reticulated_python",
+    "genus": "Python",
+    "species": "reticulatus"
+  },
+  {
+    "scientificName": "Crocodylus niloticus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Nile_crocodile",
+    "genus": "Crocodylus",
+    "species": "niloticus"
+  },
+  {
+    "scientificName": "Hippopotamus amphibius",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Hippopotamus",
+    "genus": "Hippopotamus",
+    "species": "amphibius"
+  },
+  {
+    "scientificName": "Elephas maximus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Asian_elephant",
+    "genus": "Elephas",
+    "species": "maximus"
+  },
+  {
+    "scientificName": "Giraffa camelopardalis",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Giraffe",
+    "genus": "Giraffa",
+    "species": "camelopardalis"
+  }
+];
