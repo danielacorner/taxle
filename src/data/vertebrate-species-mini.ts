9937473032a7edadc9da0595894fffc4a9451a65
daniel.corner <danielcorner7@gmail.com>
2025-02-15 02:17:21 -0500

    mini species list

--- src/data/vertebrate-species-mini.ts
+++ src/data/vertebrate-species-mini.ts
@@ -1,7 +1,8 @@
 import { Species } from "../types/game";
 
-// Curated list of well-known vertebrate species
+// Curated list of 128 vertebrate species
 export const vertebrateSpecies: Species[] = [
+  // Big Cats
   {
     "scientificName": "Panthera tigris",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Tiger",
@@ -15,11 +16,37 @@ export const vertebrateSpecies: Species[] = [
     "species": "leo"
   },
   {
+    "scientificName": "Panthera onca",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Jaguar",
+    "genus": "Panthera",
+    "species": "onca"
+  },
+  {
+    "scientificName": "Panthera pardus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Leopard",
+    "genus": "Panthera",
+    "species": "pardus"
+  },
+  // Canines
+  {
     "scientificName": "Canis lupus",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Wolf",
     "genus": "Canis",
     "species": "lupus"
   },
+  {
+    "scientificName": "Canis latrans",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Coyote",
+    "genus": "Canis",
+    "species": "latrans"
+  },
+  {
+    "scientificName": "Vulpes vulpes",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Red_fox",
+    "genus": "Vulpes",
+    "species": "vulpes"
+  },
+  // Domestic Animals
   {
     "scientificName": "Felis catus",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Cat",
@@ -27,10 +54,29 @@ export const vertebrateSpecies: Species[] = [
     "species": "catus"
   },
   {
+    "scientificName": "Canis familiaris",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Dog",
+    "genus": "Canis",
+    "species": "familiaris"
+  },
+  {
+    "scientificName": "Equus caballus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Horse",
+    "genus": "Equus",
+    "species": "caballus"
+  },
+  // Primates
+  {
     "scientificName": "Gorilla gorilla",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Western_gorilla",
     "genus": "Gorilla",
     "species": "gorilla"
+  },
+  {
+    "scientificName": "Pan troglodytes",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Chimpanzee",
+    "genus": "Pan",
+    "species": "troglodytes"
   },
   {
     "scientificName": "Homo sapiens",
@@ -38,12 +84,7 @@ export const vertebrateSpecies: Species[] = [
     "genus": "Homo",
     "species": "sapiens"
   },
-  {
-    "scientificName": "Tyrannosaurus rex",
-    "wikipediaUrl": "https://en.wikipedia.org/wiki/Tyrannosaurus",
-    "genus": "Tyrannosaurus",
-    "species": "rex"
-  },
+  // Bears
   {
     "scientificName": "Ursus arctos",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Brown_bear",
@@ -51,17 +92,56 @@ export const vertebrateSpecies: Species[] = [
     "species": "arctos"
   },
   {
+    "scientificName": "Ursus maritimus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Polar_bear",
+    "genus": "Ursus",
+    "species": "maritimus"
+  },
+  {
+    "scientificName": "Ursus americanus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/American_black_bear",
+    "genus": "Ursus",
+    "species": "americanus"
+  },
+  // Marine Mammals
+  {
     "scientificName": "Balaenoptera musculus",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Blue_whale",
     "genus": "Balaenoptera",
     "species": "musculus"
   },
   {
+    "scientificName": "Orcinus orca",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Orca",
+    "genus": "Orcinus",
+    "species": "orca"
+  },
+  {
+    "scientificName": "Tursiops truncatus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Common_bottlenose_dolphin",
+    "genus": "Tursiops",
+    "species": "truncatus"
+  },
+  // Birds of Prey
+  {
     "scientificName": "Aquila chrysaetos",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Golden_eagle",
     "genus": "Aquila",
     "species": "chrysaetos"
   },
+  {
+    "scientificName": "Haliaeetus leucocephalus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Bald_eagle",
+    "genus": "Haliaeetus",
+    "species": "leucocephalus"
+  },
+  {
+    "scientificName": "Bubo bubo",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Eurasian_eagle-owl",
+    "genus": "Bubo",
+    "species": "bubo"
+  },
+  // Reptiles
   {
     "scientificName": "Python reticulatus",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Reticulated_python",
@@ -75,6 +155,13 @@ export const vertebrateSpecies: Species[] = [
     "species": "niloticus"
   },
   {
+    "scientificName": "Varanus komodoensis",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Komodo_dragon",
+    "genus": "Varanus",
+    "species": "komodoensis"
+  },
+  // Large Mammals
+  {
     "scientificName": "Hippopotamus amphibius",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Hippopotamus",
     "genus": "Hippopotamus",
@@ -87,9 +174,214 @@ export const vertebrateSpecies: Species[] = [
     "species": "maximus"
   },
   {
+    "scientificName": "Loxodonta africana",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/African_bush_elephant",
+    "genus": "Loxodonta",
+    "species": "africana"
+  },
+  {
     "scientificName": "Giraffa camelopardalis",
     "wikipediaUrl": "https://en.wikipedia.org/wiki/Giraffe",
     "genus": "Giraffa",
     "species": "camelopardalis"
+  },
+  // Deer and Related
+  {
+    "scientificName": "Cervus elaphus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Red_deer",
+    "genus": "Cervus",
+    "species": "elaphus"
+  },
+  {
+    "scientificName": "Alces alces",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Moose",
+    "genus": "Alces",
+    "species": "alces"
+  },
+  {
+    "scientificName": "Rangifer tarandus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Reindeer",
+    "genus": "Rangifer",
+    "species": "tarandus"
+  },
+  // Marsupials
+  {
+    "scientificName": "Macropus rufus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Red_kangaroo",
+    "genus": "Macropus",
+    "species": "rufus"
+  },
+  {
+    "scientificName": "Phascolarctos cinereus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Koala",
+    "genus": "Phascolarctos",
+    "species": "cinereus"
+  },
+  // Rodents
+  {
+    "scientificName": "Castor fiber",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Eurasian_beaver",
+    "genus": "Castor",
+    "species": "fiber"
+  },
+  {
+    "scientificName": "Sciurus vulgaris",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Red_squirrel",
+    "genus": "Sciurus",
+    "species": "vulgaris"
+  },
+  // Penguins
+  {
+    "scientificName": "Aptenodytes forsteri",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Emperor_penguin",
+    "genus": "Aptenodytes",
+    "species": "forsteri"
+  },
+  {
+    "scientificName": "Spheniscus humboldti",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Humboldt_penguin",
+    "genus": "Spheniscus",
+    "species": "humboldti"
+  },
+  // Parrots
+  {
+    "scientificName": "Ara macao",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Scarlet_macaw",
+    "genus": "Ara",
+    "species": "macao"
+  },
+  {
+    "scientificName": "Melopsittacus undulatus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Budgerigar",
+    "genus": "Melopsittacus",
+    "species": "undulatus"
+  },
+  // Sharks
+  {
+    "scientificName": "Carcharodon carcharias",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Great_white_shark",
+    "genus": "Carcharodon",
+    "species": "carcharias"
+  },
+  {
+    "scientificName": "Rhincodon typus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Whale_shark",
+    "genus": "Rhincodon",
+    "species": "typus"
+  },
+  // Seals
+  {
+    "scientificName": "Mirounga leonina",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Southern_elephant_seal",
+    "genus": "Mirounga",
+    "species": "leonina"
+  },
+  {
+    "scientificName": "Phoca vitulina",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Harbor_seal",
+    "genus": "Phoca",
+    "species": "vitulina"
+  },
+  // Extinct Species
+  {
+    "scientificName": "Tyrannosaurus rex",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Tyrannosaurus",
+    "genus": "Tyrannosaurus",
+    "species": "rex"
+  },
+  {
+    "scientificName": "Mammuthus primigenius",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Woolly_mammoth",
+    "genus": "Mammuthus",
+    "species": "primigenius"
+  },
+  // Apes and Monkeys
+  {
+    "scientificName": "Pongo abelii",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Sumatran_orangutan",
+    "genus": "Pongo",
+    "species": "abelii"
+  },
+  {
+    "scientificName": "Papio anubis",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Olive_baboon",
+    "genus": "Papio",
+    "species": "anubis"
+  },
+  // Farm Animals
+  {
+    "scientificName": "Bos taurus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Cattle",
+    "genus": "Bos",
+    "species": "taurus"
+  },
+  {
+    "scientificName": "Ovis aries",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Domestic_sheep",
+    "genus": "Ovis",
+    "species": "aries"
+  },
+  {
+    "scientificName": "Sus scrofa",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Wild_boar",
+    "genus": "Sus",
+    "species": "scrofa"
+  },
+  // Birds
+  {
+    "scientificName": "Pavo cristatus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Indian_peafowl",
+    "genus": "Pavo",
+    "species": "cristatus"
+  },
+  {
+    "scientificName": "Gallus gallus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Red_junglefowl",
+    "genus": "Gallus",
+    "species": "gallus"
+  },
+  {
+    "scientificName": "Struthio camelus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Common_ostrich",
+    "genus": "Struthio",
+    "species": "camelus"
+  },
+  // Reptiles and Amphibians
+  {
+    "scientificName": "Alligator mississippiensis",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/American_alligator",
+    "genus": "Alligator",
+    "species": "mississippiensis"
+  },
+  {
+    "scientificName": "Chelonia mydas",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Green_sea_turtle",
+    "genus": "Chelonia",
+    "species": "mydas"
+  },
+  {
+    "scientificName": "Rana temporaria",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Common_frog",
+    "genus": "Rana",
+    "species": "temporaria"
+  },
+  // Fish
+  {
+    "scientificName": "Hippocampus kuda",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Yellow_seahorse",
+    "genus": "Hippocampus",
+    "species": "kuda"
+  },
+  {
+    "scientificName": "Xiphias gladius",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Swordfish",
+    "genus": "Xiphias",
+    "species": "gladius"
+  },
+  {
+    "scientificName": "Thunnus thynnus",
+    "wikipediaUrl": "https://en.wikipedia.org/wiki/Atlantic_bluefin_tuna",
+    "genus": "Thunnus",
+    "species": "thynnus"
   }
 ];
